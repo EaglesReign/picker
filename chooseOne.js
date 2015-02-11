15d4c47a125627d9260e6bf994db1094253068da
Phillip <phv9495@saintjoe.edu>
2015-02-10 20:45:12 -0500

    Added/changed comments to better explain what the program now does. Also, the logic works perfectly. I switched the names of people for numbers and the program will pick a number between 1 and the number of "sides" on the die.

--- chooseOne.js
+++ chooseOne.js
@@ -1,13 +1,26 @@
-var dice = [['Austgen','Grube','Heinzman','Herre','Hofer','Samano','Zekovic'],['Davis', 'Gainey', 'Klump', 'Postma']];
+//var dice = [['Austgen','Grube','Heinzman','Herre','Hofer','Samano','Zekovic'],['Davis', 'Gainey', 'Klump', 'Postma']];
 
-var dice = { "cmp237": ['Craig Austgen','Daniel Davis','Benjamin Davisson','William Decker','Zachary Dickinson','Katie Harold','Eric Heinzman','Phillip Hofer','Troy Key','Andrew Klump','Kyle Lowtharp','Collin Mullis','Gabriel Perez','Ryan Postma','Kevin Regner','Andrew Ribicki','Connor Ring','Nathan Samano','Griffin Scott','Jared Smith'], "cmp112": ['Thaddeus Albert','Dan Amador','Bruce Anderson','Zachary Aring','Benjamin Davisson','Jace Horner','Carl Jones','Kyle Lowtharp','Declan McCarron','Collin Mullis','Yemi Ogunbameru','Andrew Ribicki','Christian Sexton','Houston Shimala','Ryan Slipher','Jared Smith','Ashlee Troop'], "cmp344": ['Craig Austgen','Daniel Davis','Dylan Gainey','Lucas Grube','Eric Heinzman','Katelyn Herre','Phillip Hofer','Andrew Klump','Ryan Postma','Nathan Samano','Danilo Zekovic'], "cmp341": ['Craig Austgen','Lucas Grube','Eric Heinzman','Phillip Hofer','Nathan Samano','Danilo Zekovic']};
+//var dice = { "D6": ['Craig Austgen','Daniel Davis','Benjamin Davisson','William Decker','Zachary Dickinson','Katie Harold','Eric Heinzman','Phillip Hofer','Troy Key','Andrew Klump','Kyle Lowtharp','Collin Mullis','Gabriel Perez','Ryan Postma','Kevin Regner','Andrew Ribicki','Connor Ring','Nathan Samano','Griffin Scott','Jared Smith'], "D10": ['Thaddeus Albert','Dan Amador','Bruce Anderson','Zachary Aring','Benjamin Davisson','Jace Horner','Carl Jones','Kyle Lowtharp','Declan McCarron','Collin Mullis','Yemi Ogunbameru','Andrew Ribicki','Christian Sexton','Houston Shimala','Ryan Slipher','Jared Smith','Ashlee Troop'], "D12": ['Craig Austgen','Daniel Davis','Dylan Gainey','Lucas Grube','Eric Heinzman','Katelyn Herre','Phillip Hofer','Andrew Klump','Ryan Postma','Nathan Samano','Danilo Zekovic'], "D20": ['Craig Austgen','Lucas Grube','Eric Heinzman','Phillip Hofer','Nathan Samano','Danilo Zekovic']};
+
+//declare dice maps, D6, D10, D12, D20
+var dice = { "D6":
+        ['1','2','3','4','5','6'], "D10":
+        ['1','2','3','4','5','6','7','8','9','10'], "D12":
+        ['1','2','3','4','5','6','7','8','9','10','11', '12'], "D20":
+        ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16',
+'17','18','19','20']}
  
-
+//chooses a random number from the chosen dice
 function chooseOne(die) {
   var thisDie = dice[die];
 
   var n = thisDie.length;
   var which = Math.floor(Math.random() * n);
 
-  print('Chosen: ' + thisDie[which]);
+  print('The Die Roll Is: ' + thisDie[which]);
 }
+//Test each die
+chooseOne("D6");
+chooseOne("D10");
+chooseOne("D12");
+chooseOne("D20");
